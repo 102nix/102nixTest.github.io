5c888724ee41cf7437aec8c238d919917bd18c09
102nix <58480558+102nix@users.noreply.github.com>
2020-09-11 16:59:32 +0500

    Add files via upload

---
+++ precache-manifest.1a84e4803b204f02b868658ac75634de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6278f8dff9d29c4c42a10047cf5dff86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29311a8f335eacb0576c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b3b263dcedc29ff27c7e",
+    "url": "/static/css/main.453721c0.chunk.css"
+  },
+  {
+    "revision": "29311a8f335eacb0576c",
+    "url": "/static/js/2.595d92f4.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.595d92f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b263dcedc29ff27c7e",
+    "url": "/static/js/main.3b62f49c.chunk.js"
+  },
+  {
+    "revision": "8fe84a25fe1a4ec5dbc5",
+    "url": "/static/js/runtime-main.280cd61d.js"
+  }
+]);
